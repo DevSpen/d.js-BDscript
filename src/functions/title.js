0ed10b2229e22ad8a8eb1357359f55d8df1c0af5
root <root@35k9.l.time4vps.cloud>
2021-04-25 22:07:15 +0300

    fixed title.js

--- src/functions/title.js
+++ src/functions/title.js
@@ -9,7 +9,7 @@ module.exports = {
             
             d.container.embed.setTitle(text)
         } else {
-            d.containter.embed.setTitle(d.value.inside) 
+            d.container.embed.setTitle(d.value.inside) 
         }
         
         return d.deflate(d.value.id, "")
