6bd185c6bce7d02aab7119bcea0acd71c0a24166
Spen <69215413+DevSpen@users.noreply.github.com>
2021-06-03 10:45:25 -0400

    Create mfaLevel.js

---
+++ src/functions/mfaLevel.js
@@ -0,0 +1,29 @@
+module.exports = {
+    name: "$mfaLevel",
+    description: "Returns a server’s MFA level.",
+    brackets: true,
+    optional: true,
+    fields: [
+        {
+            name: "guildID",
+            description: "The server to return the MFA level of.",
+            type: "string"
+        }
+    ],
+    returns: "string",
+    execute: async d => {
+        if (d.value.inside) {
+            const guildID = await d.resolveAll()
+
+            if (guildID === undefined) return undefined
+        
+            const guild = d.client.guilds.cache.get(guildID)
+            
+            if (!guild) return d.sendError("guildID", guildID)
+
+            return d.deflate(guild.mfaLevel)
+        } else {
+            return d.deflate(d.message?.guild?.mfaLevel ?? 0)
+        }
+    }
+}
