22c49dc1e1d8e99924545a8162fc9742df176470
Spen <69215413+DevSpen@users.noreply.github.com>
2021-06-03 16:22:21 -0400

    New Function: $isNaN

---
+++ src/functions/isNaN.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "$isNaN",
+    brackets: true,
+    description: "Returns whether the provided text is not a number (NaN).",
+    fields: [{
+        name: "text",
+        type: "string",
+        description: "The string to check."
+    }],
+    returns: "boolean", 
+    execute: async d => {
+        const code = await d.resolveAll()
+
+        if (code === undefined) return undefined
+
+        return d.deflate(code.isNaN())
+    }
+}
