3d689b62ff14bb417ba23b26e6c7dda0cb52b780
Spen <69215413+DevSpen@users.noreply.github.com>
2021-06-05 07:30:56 -0400

    New Function: $serverBoostingSince

---
+++ src/functions/serverBoostingSince.js
@@ -0,0 +1,38 @@
+module.exports = {
+     name: "$serverBoostingSince",
+     description: "Returns how long a user has been boosting in milliseconds.",
+     returns: "?number",
+     brackets: true,
+     optional: true,
+     fields: [{
+         name: "guildID",
+         description: "The server to get the data from.",
+         type: "string"
+     }, {
+         name: "userID",
+         description: "The user to get the data for.",
+         type: "string"
+     }],
+     execute: async d => {
+         if (!d.value.inside) {
+             return d.deflate(d.message?.member?.premiumSinceTimestamp ?? "")
+         }
+
+         const [
+             guildID,
+             userID
+         ] = (await d.resolveArray()) || []
+
+         if (guildID === undefined) return 
+
+         const guild = d.client.guilds.cache.get(guildID)
+
+         if (!guild) return d.sendError("guildID", guildID)
+
+         const member = await guild.members.fetch(userID || "1").catch(err => null)
+
+         if (!member) return d.sendError("userID", userID)
+
+         return d.deflate(member.premiumSinceTimestamp)
+     }
+ }
