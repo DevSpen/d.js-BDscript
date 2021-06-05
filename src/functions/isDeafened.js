3ac194563a63f88d0e28bc3046f0ecf24145bad5
Spen <69215413+DevSpen@users.noreply.github.com>
2021-06-05 09:17:56 -0400

    New Function $isDeafened

---
+++ src/functions/isDeafened.js
@@ -0,0 +1,38 @@
+module.exports = {
+    name: "$isDeafened",
+    description: "Returns whether or not a user is deafened. Applies to both self and server deafen.",
+    returns: "boolean",
+    brackets: true,
+    optional: true,
+    fields: [{
+        name: "guildID",
+        description: "The server to get the data from.",
+        type: "string"
+    }, {
+        name: "userID",
+        description: "The user to get the data for.",
+        type: "string"
+    }],
+    execute: async d => {
+        if (!d.value.inside) {
+            return d.deflate(d.message?.member?.voice.deaf ?? "")
+        }
+
+        const [
+            guildID,
+            userID
+        ] = (await d.resolveArray()) || []
+
+        if (guildID === undefined) return 
+
+        const guild = d.client.guilds.cache.get(guildID)
+
+        if (!guild) return d.sendError("guildID", guildID)
+
+        const member = await guild.members.fetch(userID || "1").catch(err => null)
+
+        if (!member) return d.sendError("userID", userID)
+
+        return d.deflate(member.voice.deaf)
+    }
+}
