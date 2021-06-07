3ee107bd8c766fd033baa0cf4de41c6c056f6cee
Spen <69215413+DevSpen@users.noreply.github.com>
2021-06-07 12:35:53 -0400

    New Function: $serverRulesChannel

---
+++ src/functions/serverRulesChannel.js
@@ -0,0 +1,29 @@
+module.exports = {
+    name: "$serverRulesChannel",
+    description: "Returns a server’s rules channel ID (which is set in community settings).",
+    brackets: true,
+    optional: true,
+    fields: [
+        {
+            name: "guildID",
+            description: "The server to return the data for.",
+            type: "?string"
+        }
+    ],
+    returns: "string",
+    execute: async d => {
+        if (d.value.inside) {
+            const guildID = await d.resolveAll()
+
+            if (guildID === undefined) return undefined
+        
+            const guild = d.client.guilds.cache.get(guildID)
+            
+            if (!guild) return d.sendError("guildID", guildID)
+
+            return d.deflate(guild.rulesChannelID)
+        } else {
+            return d.deflate(d.message?.guild?.rulesChannelID ?? "")
+        }
+    }
+}
