ceeea585eb35fbf8e61c10375d99e4b4f9a5064c
Spen <69215413+DevSpen@users.noreply.github.com>
2021-06-07 13:07:27 -0400

    New Function: $serverPreferredLocale

---
+++ src/functions/serverPreferredLocale.js
@@ -0,0 +1,29 @@
+module.exports = {
+    name: "$serverPreferredLocale",
+    description: "Returns a server's preferred locale.",
+    brackets: true,
+    optional: true,
+    fields: [
+        {
+            name: "guildID",
+            description: "The server to return the data for.",
+            type: "string"
+        }
+    ],
+    returns: "?string",
+    execute: async d => {
+        if (d.value.inside) {
+            const guildID = await d.resolveAll()
+
+            if (guildID === undefined) return undefined
+        
+            const guild = d.client.guilds.cache.get(guildID)
+            
+            if (!guild) return d.sendError("guildID", guildID)
+
+            return d.deflate(guild.preferredLocale)
+        } else {
+            return d.deflate(d.message?.guild?.preferredLocale ?? "")
+        }
+    }
+}
