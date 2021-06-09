13c481559c037bfd04883d02931a9fbd1c22742a
Spen <69215413+DevSpen@users.noreply.github.com>
2021-06-09 14:42:08 -0400

    New Function: $getNickname
    Returns a user’s nickname. If the user doesn't have a nickname, nothing is returned. (Unlike $nickname, which returns the user's username if they don't have a nickname.)

---
+++ src/functions/getNickname.js
@@ -0,0 +1,38 @@
+module.exports = {
+    name: "$getNickname",
+    description: "Returns a user’s nickname. If the user doesn't have a nickname, nothing is returned. (Unlike $nickname, which returns the user's username if they don't have a nickname.)",
+    returns: "?string",
+    brackets: true,
+    optional: true,
+    fields: [{
+        name: "guildID",
+        description: "The server to get the nickname from.",
+        type: "string"
+    }, {
+        name: "userID",
+        description: "The user to get the nickname for.",
+        type: "string"
+    }],
+    execute: async d => {
+        if (!d.value.inside) {
+            return d.deflate(d.message?.member?.nickname ?? "")
+        }
+        
+        const [
+            guildID,
+            userID
+        ] = (await d.resolveArray()) || []
+        
+        if (guildID === undefined) return 
+        
+        const guild = d.client.guilds.cache.get(guildID)
+        
+        if (!guild) return d.sendError("guildID", guildID)
+        
+        const member = await guild.members.fetch(userID || "1").catch(err => null)
+        
+        if (!member) return d.sendError("userID", userID)
+        
+        return d.deflate(member.nickname)
+    }
+}
