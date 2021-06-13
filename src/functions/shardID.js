9cffb655890f7f5dd9892c27a8e8ccffec2d8422
Spen <69215413+DevSpen@users.noreply.github.com>
2021-06-13 14:23:20 -0400

    Added a guildID option to $shardID

--- src/functions/shardID.js
+++ src/functions/shardID.js
@@ -1,8 +1,29 @@
 module.exports = {
     name: "$shardID",
-    returns: "?number",
-    description: "the shard this guild is in",
-    execute: async d =>{
-        return d.deflate(d.message?.guild?.shardID ?? "")
+    description: "Returns what shard a server is on. (if sharding is active)",
+    brackets: true,
+    optional: true,
+    fields: [
+        {
+            name: "guildID",
+            description: "The server to return the data for.",
+            type: "string"
+        }
+    ],
+    returns: "?string",
+    execute: async d => {
+        if (d.value.inside) {
+            const guildID = await d.resolveAll()
+
+            if (guildID === undefined) return undefined
+
+            const guild = d.client.guilds.cache.get(guildID)
+
+            if (!guild) return d.sendError("guildID", guildID)
+
+            return d.deflate(guild.shardID)
+        } else {
+            return d.deflate(d.message?.guild?.shardID ?? "")
+        }
     }
 }
