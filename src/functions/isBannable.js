cfe017e9ac41e05cd690ecc1d7b73a3d6082ee68
Spen <69215413+DevSpen@users.noreply.github.com>
2021-06-17 08:42:40 -0400

    Create isBannable.js

---
+++ src/functions/isBannable.js
@@ -0,0 +1,38 @@
+module.exports = {
+    name: "$isBannable",
+    description: "Whether or not the bot can ban the provided user. According to permissions and hierarchy (e.g role position and server ownership).",
+    returns: "boolean",
+    brackets: true,
+    optional: true,
+    fields: [{
+        name: "guildID",
+        description: "The server to get the data from.",
+        type: "string"
+    }, {
+        name: "userID",
+        description: "The user to get the data for.",
+        type: "string"
+    }],
+    execute: async d => {
+        if (!d.value.inside) {
+            return d.deflate(d.message?.member?.bannable ?? "")
+        }
+
+        const [
+            guildID,
+            userID
+        ] = (await d.resolveArray()) || []
+
+        if (guildID === undefined) return 
+
+        const guild = d.client.guilds.cache.get(guildID)
+
+        if (!guild) return d.sendError("guildID", guildID)
+
+        const member = await guild.members.fetch(userID || "1").catch(err => null)
+
+        if (!member) return d.sendError("userID", userID)
+
+        return d.deflate(member.bannable)
+    }
+}
